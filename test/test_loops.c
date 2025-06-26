713119ea85b74010efda71f65a224b4207e2e10c
danusitacm <danielacristaldomo@gmail.com>
2025-06-26 20:09:38 -0300

    Elimina funcion main en test-loops.c

--- test/test_loops.c
+++ test/test_loops.c
@@ -2,22 +2,20 @@
 // Prueba while y for loops
 
 int i, sum, count;
+sum = 0;
+count = 10;
 
-void main() {
-    sum = 0;
-    count = 10;
-    
-    // While loop simple
-    while (count > 0) {
-        sum = sum + count;
-        count = count - 1;
-    }
-
-    // While loop con condición compleja
-    i = 1;
-    while (i <= 5 && sum < 100) {
-        sum = sum + i * 2;
-        i = i + 1;
-    }
-
+// While loop simple
+while (count > 0) {
+    sum = sum + count;
+    count = count - 1;
 }
+
+// While loop con condición compleja
+i = 1;
+while (i <= 5 && sum < 100) {
+    sum = sum + i * 2;
+    i = i + 1;
+}
+
+
